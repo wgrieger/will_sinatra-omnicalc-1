b426b8bb564889113f4a7919859e9a9152466957
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-04-17 03:29:55 +0000

    payment_formula

--- app.rb
+++ app.rb
@@ -30,6 +30,21 @@ get("/payment/new") do
     
 end
 
+get("/payment/results")do
+@apr_input=params.fetch(user_apr).to_f
+@years_input_preconversion=params.fetch(user_years).to_f
+@principal_input=params.fetch(user_pv).to_f
+@years_input_months=@years_input_preconversion*12
+
+
+@r=@apr_input/100/12
+@part_denominator=(1+@r)**(-@years_input_months)
+
+@payment_output= (@r*@principal_input)/(1-@part_denominator)
+
+  erb(:payment_results)
+end
+
 #random
 get("/random/new") do
   erb(:random)
--- test.rb
+++ test.rb
@@ -1,3 +1,12 @@
-testing= rand(10.0..15.0)
-output= testing.round(15)
-pp output
+@apr_input=params.fetch(user_apr).to_f
+@years_input_preconversion=params.fetch(user_years).to_f
+@principal_input=params.fetch(user_pv).to_f
+@years_input_months=@years_input_preconversion*12
+
+
+@r=@apr_input/100/12
+@part_denominator=(1+@r)**(-@years_input_months)
+
+@payment_output= (@r*@principal_input)/(1-@part_denominator)
+
+pp @payment_output
