ccc9def8bd6681d73a0c7cf332a23f7562cd95fb
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-04-17 02:44:07 +0000

    square_is_done

--- app.rb
+++ app.rb
@@ -5,6 +5,14 @@ get("/square/new") do
   erb(:square)
 end
 
+get("/square/results")do
+@input_sqr=params.fetch(:number).to_f
+@result_sqr= @input_sqr*@input_sqr
+
+erb(:square_results)
+end
+
+#square root
 get("/square_root/new") do
   erb(:sqrt)
 end
---
+++ test.rb
@@ -0,0 +1,3 @@
+sqr=4
+outcome = sqr * sqr
+pp outcome
